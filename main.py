a5b9168f61649b0afb0937595041dd1d09a52672
diadiawowka <v.kucinsky@gmail.com>
2024-01-29 14:39:04 +0300

    v 1.0.0

---
+++ main.py
@@ -0,0 +1,43 @@
+
+# a = 5
+# b = 5.89
+# c = 'hello'
+
+# print (f"{a} - {b} - {c}")
+
+# print('Введите первое число')
+# a = int(input())
+
+# b = int(input('Введите 2е число'))
+
+# print(a, '//',b, '=', a//b)
+
+# a = 5.894758
+# b = 6.558585
+
+# print(round(a*b, 2))
+
+# n = int(input())
+# flag = True
+# i =2
+# while flag:
+#     if n % i == 0:
+#         flag = False
+#         print(i)
+#     elif i > n // 2:
+#         print(n)
+#         flag = False
+#     i += 1
+
+# a = 'qwerty'
+
+# for i in a:
+#     print(i)
+
+# line = ""
+# for i in range(5):
+#     line = ""
+#     for j in range(5):
+#         line += "+"
+#     print(line)
+
